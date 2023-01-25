541ff92f599e3692c916f095a315aed0fb8862bc
alexandrebelem21 <alexandrebelem21@gmail.com>
2023-01-25 00:38:24 -0300

    Feat: Project3 Wallet

--- src/components/Project3.js
+++ src/components/Project3.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import github from '../images/github.png';
+import wallet from '../images/wallet.png';
 
 function Project3() {
+  const goProject = () => {
+    window.location.assign('https://wallet-alexandrebelem21.vercel.app/');
+  };
   return (
     <div className="mt-4">
       <div className="text-black text-6xl h-screen w-[100vw]
     grid grid-rows-3 md:grid-rows-2 divide-y md:divide-y-0  md:mt-[25%]
-
-     md:ml-[18vw]
+    mt-[25vh]
+    md:ml-[18vw]
     md:w-2/3  md:grid-cols-8 md:divide-x  divide-neutral-500 "
       >
 
         <div className=" pl-12 md:pl-0 text-5xl text-neutral-200 w-[100vw] md:w-[25vw]
       md:col-start-1 md:col-end-4 flex items-end md:items-start md:mx-auto"
         >
-          <div className=" text-5x text-neutral-200 col-start-1 col-end-4
+          <div className=" text-3xl md:text-5xl text-neutral-200 col-start-1 col-end-4
           text-center md:text-right pr-8 mx-auto "
           >
-            Project Three
-            <div className=" text-base text-neutral-500 mt-6">
+            Project Wallet
+            <div className=" text-sm md:text-base text-neutral-500 mt-6">
               { (window.screen.width < 700)
                 ? (
                   <div>
                     <p>
-                      {`Este projeto consiste em um app de receitas teste teste teste teste.
-                      Este projeto consiste 
+                      {`Consiste em uma carteira virtual que permite gerenciar despesas.
+                      Uma consulta a uma api é feita para fazer a conversao par ao Real.
+                      Foi usado ReactJs com redux e o bootstrap.
+                      
+                      `}
+                    </p>
+                    <p>
+                      {`
+                      Ao clicar na imagem abaixo é possivel ver a aplicação funcionando.
                       `}
                     </p>
                   </div>
@@ -33,23 +43,22 @@ function Project3() {
                   <div>
                     <p>
                       {`
-                  Este projeto consiste em um app de receitas teste teste teste testeEste 
-                  projeto consiste em um app de receitas teste teste teste testeEste projeto 
-                  consiste em um app de receitas teste teste teste 
-                  testeEste projeto consiste em um app de receitas teste teste teste teste `}
+                 Consiste em uma carteira virtual que permite adicionar, remover ou ainda editar despesas.
+                 Na primeira pagina é nescessario fazer um login, com um email valido.
+                 `}
                     </p>
+
                     <p>
-                      {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                      Ut feugiat orci vitae dui fringilla pretium. Suspendisse aliquam, 
-                      dolor sit amet sagittis aliquam, 
-                      dolor leo scelerisque lacus, a aliquam dui ligula sit amet mauris.  `}
+                      {`Atraves de uma api é possivel ver o valor lançado nas mais pouplares moedas e a conversão para o real.
+                      `}
                     </p>
                     <br />
+                    <p>
+                      {`Foi desenvolvido usando o React com redux e para auxiliar na estilizaçao foi usado o bootstrap. 
+                      `}
+                    </p>
                   </div>
                 )}
-              <div>
-                Icone das linguagens usadas aqui
-              </div>
             </div>
           </div>
         </div>
@@ -60,7 +69,12 @@ function Project3() {
       w-[100vw]
         flex md:h-screen justify-center items-top"
         >
-          <img src={github} className="md:h-[50vh] mt-8 h-[30vh] w-[100vw] mx-auto pr-8 " alt="Minha imagem" />
+          <button
+            type="button"
+            onClick={() => goProject()}
+          >
+            <img src={wallet} className="md:h-[50vh] mt-8 h-[30vh] w-[100vw] mx-auto pr-8 " alt="Minha imagem" />
+          </button>
         </div>
       </div>
       <div className="relative text-base md:text-2xl md:rotate-90
@@ -70,7 +84,7 @@ function Project3() {
         <p className="text-gray-700 md:mb-4">About</p>
         <p className="text-gray-700 md:mb-4">Project 1</p>
         <p className="text-gray-700 md:mb-4">Project 2</p>
-        <p className="text-gray-500 md:mb-4">Project 3</p>
+        <p className="text-gray-500 md:mb-4">App_Wallet</p>
         <p className="text-gray-700 md:mb-4">Contact</p>
       </div>
     </div>
